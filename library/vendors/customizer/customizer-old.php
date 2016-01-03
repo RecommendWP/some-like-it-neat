eb60e98a1295303f683d91ddbd52f0ea6df2fbba
alex vasquez <alexjvas@gmail.com>
2016-01-02 23:39:58 -0800

    rename bootstrap file to customizer.php for consistency

---
+++ library/vendors/customizer/customizer-old.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * some_like_it_neat Theme Customizer
+ *
+ * @package some_like_it_neat
+ * Look, I'm sorry about this messy file, mmkay? Saw Paul Underwood's  repo for extended customizer controls and had a
+ * "moment." Okay? Below, just copy the controls you need to use and go bonkers, customize as needed. I started things
+ * off by adding a Textarea for GA.
+ */
+
+
+
+
+
+
+function some_like_it_neat_add_customizer_theme_options($wp_customize) {
+
+
+}
+add_action('customize_register', 'some_like_it_neat_add_customizer_theme_options');
